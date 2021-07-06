968841d3da5d83f6d9407fef6551d78823d1d016
Vincomer <98926599+Vincomer@users.noreply.github.com>
2021-07-06 07:38:00 +0000

    Update git ignore

--- config/application.rb
+++ config/application.rb
@@ -15,4 +15,11 @@ module TrafficReport
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
-    # Only load the plugins named here, i
+    # Only load the plugins named here, in the order given (default is alphabetical).
+    # :all can be used as a placeholder for all plugins not explicitly named.
+    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+
+    # Activate observers that should always be running.
+    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+
+    # Set Time.zone default to the s

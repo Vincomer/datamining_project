178ccc25f6fa2a0a474aa3eca21cba99e38dba11
Vincomer <98926599+Vincomer@users.noreply.github.com>
2021-07-06 21:47:26 +0000

    Deploying Gravitee.io dev repos Circle CI Pipeline config

--- config/application.rb
+++ config/application.rb
@@ -36,4 +36,7 @@ module TrafficReport
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
-    # Configure sensitive parameters which will be filtered from the 
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
+  end
+end
---
+++ config/environments/development.rb
@@ -0,0 +1,2 @@
+TrafficReport::Application.configure do
+  # Settings specified here will take precedence over those in config/ap

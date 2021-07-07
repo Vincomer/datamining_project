b3aa488d5747860d52903688c13664caf1ce2c6b
Vincomer <98926599+Vincomer@users.noreply.github.com>
2021-07-07 21:43:51 +0000

    Revert previous change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,4 +2,15 @@ TrafficReport::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # The production environment is meant for finished, "live" apps.
-  # Code is not reloaded betwee
+  # Code is not reloaded between requests
+  config.cache_classes = true
+
+  # Full error reports are disabled and caching is turned on
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+
+  # Specifies the header that your server uses for sending files
+  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+
+  # For nginx:
+  # config.action_dispatch.x_sendfile_header =

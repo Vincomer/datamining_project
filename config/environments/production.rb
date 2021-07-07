deb1bdb011c0e95135e4afa6f93edbdca8d2c2c3
Vincomer <98926599+Vincomer@users.noreply.github.com>
2021-07-07 14:00:25 +0000

    Update wt.js

---
+++ config/environments/production.rb
@@ -0,0 +1,5 @@
+TrafficReport::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb
+
+  # The production environment is meant for finished, "live" apps.
+  # Code is not reloaded betwee

df1a1f67c23429c793c79878f59c608be6f8b0ad
Vincomer <98926599+Vincomer@users.noreply.github.com>
2021-07-07 09:23:55 +0000

    Bump eslint from 7.26.0 to 7.27.0

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,4 +7,14 @@ TrafficReport::Application.configure do
   config.cache_classes = false
 
   # Log error messages when you accidentally call methods on nil.
-  conf
+  config.whiny_nils = true
+
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = true
+  config.action_view.debug_rjs             = true
+  config.action_controller.perform_caching = false
+
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
+
+  # Print deprecation notices to the Rai

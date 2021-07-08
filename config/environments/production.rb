73ea95d82be9a5efe7adc6b4737b184660999eac
Vincomer <98926599+Vincomer@users.noreply.github.com>
2021-07-08 09:05:51 +0000

    chore(ci): update actions/setup-node action to v2.1.5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,4 +13,16 @@ TrafficReport::Application.configure do
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   # For nginx:
-  # config.action_dispatch.x_sendfile_header =
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+
+  # If you have no front-end server that supports something like X-Sendfile,
+  # just comment this out and Rails will serve the files
+
+  # See everything in the log (default is :info)
+  # config.log_level = :debug
+
+  # Use a different logger for distributed setups
+  # config.logger = SyslogLogger.new
+
+  # Use a different cache store in production
+  # config.ca

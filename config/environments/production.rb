7b2ffc7e821e7fd57398e384d51afad3b2e9f50b
Vincomer <98926599+Vincomer@users.noreply.github.com>
2021-07-09 14:52:56 +0000

    refactor packages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,4 +35,15 @@ TrafficReport::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Disable delivery errors, bad email addresses will be ignored
-  # confi
+  # config.action_mailer.raise_delivery_errors = false
+
+  # Enable threaded mode
+  # config.threadsafe!
+
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation can not be found)
+  config.i18n.fallbacks = true
+
+  # Send deprecation notices to registered listeners
+  config.active_support.deprecation = :notify
+end

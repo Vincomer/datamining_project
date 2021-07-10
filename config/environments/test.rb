da5a37a4d34510cb28cda0be975cc8a7e0e18c85
Vincomer <98926599+Vincomer@users.noreply.github.com>
2021-07-10 06:41:36 +0000

    libceph: add update_authorizer auth method

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -4,4 +4,14 @@ TrafficReport::Application.configure do
   # The test environment is used exclusively to run your application's
   # test suite.  You never need to work with it otherwise.  Remember that
   # your test database is "scratch space" for the test suite and is wiped
-  # and recreated between test runs.
+  # and recreated between test runs.  Don't rely on the data there!
+  config.cache_classes = true
+
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
+
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+
+  # Raise exceptions instead of rendering exception templat

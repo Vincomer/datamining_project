5619734d1b67ad488b11796314b1e85957e30151
Vincomer <98926599+Vincomer@users.noreply.github.com>
2021-07-14 16:38:45 +0000

    ALSA: Add a reference counter to card instance

--- db/schema.rb
+++ db/schema.rb
@@ -5,4 +5,14 @@
 # Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
-# from scratch.
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20110717144357) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "url"
+    t.string   "category"
+    t.integer  "post_id"

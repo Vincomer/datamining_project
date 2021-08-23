6511ddd7616808d37a42529307ead6489c3440a3
Vincomer <98926599+Vincomer@users.noreply.github.com>
2021-08-23 05:51:44 +0000

    added license.txt

--- public/javascripts/effects.js
+++ public/javascripts/effects.js
@@ -1,1 +1,12 @@
-// script.aculo.us effects.js v1.8.3, Thu Oct 08 11:23:33 +02
+// script.aculo.us effects.js v1.8.3, Thu Oct 08 11:23:33 +0200 2009
+
+// Copyright (c) 2005-2009 Thomas Fuchs (http://script.aculo.us, http://mir.aculo.us)
+// Contributors:
+//  Justin Palmer (http://encytemedia.com/)
+//  Mark Pilgrim (http://diveintomark.org/)
+//  Martin Bialasinki
+//
+// script.aculo.us is freely distributable under the terms of an MIT-style license.
+// For details, see the script.aculo.us web site: http://script.aculo.us/
+
+// converts

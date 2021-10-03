31430978b73b5b1dc5163b0e567fa7acb2c0f8bb
Vincomer <98926599+Vincomer@users.noreply.github.com>
2021-10-03 12:06:13 +0000

    Commit initial

--- public/javascripts/prototype.js
+++ public/javascripts/prototype.js
@@ -1,4 +1,16 @@
 /*  Prototype JavaScript framework, version 1.7_rc2
  *  (c) 2005-2010 Sam Stephenson
  *
- *  Prototype is freely distribu
+ *  Prototype is freely distributable under the terms of an MIT-style license.
+ *  For details, see the Prototype web site: http://www.prototypejs.org/
+ *
+ *--------------------------------------------------------------------------*/
+
+var Prototype = {
+
+  Version: '1.7_rc2',
+
+  Browser: (function(){
+    var ua = navigator.userAgent;
+    var isOpera = Object.prototype.toString.call(window.opera) == '[object Opera]';
+    retur

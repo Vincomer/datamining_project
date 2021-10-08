191991e394a51ecce12459f8c93466da684b9ed7
Vincomer <98926599+Vincomer@users.noreply.github.com>
2021-10-08 07:49:42 +0000

    Btrfs: fix fs corruption on transaction abort if device supports discard

--- public/javascripts/prototype.js
+++ public/javascripts/prototype.js
@@ -98,4 +98,18 @@ var Class = (function() {
       parent = properties.shift();
 
     function klass() {
-      th
+      this.initialize.apply(this, arguments);
+    }
+
+    Object.extend(klass, Class.Methods);
+    klass.superclass = parent;
+    klass.subclasses = [];
+
+    if (parent) {
+      subclass.prototype = parent.prototype;
+      klass.prototype = new subclass;
+      parent.subclasses.push(klass);
+    }
+
+    for (var i = 0, length = properties.length; i < length; i++)
+      klass.addMeth

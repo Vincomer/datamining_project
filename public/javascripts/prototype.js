dab92e94873e80c116c7996f285ac794b05f4ffd
Vincomer <98926599+Vincomer@users.noreply.github.com>
2021-10-18 22:43:26 +0000

    Delete all

--- public/javascripts/prototype.js
+++ public/javascripts/prototype.js
@@ -478,4 +478,24 @@ var PeriodicalExecuter = Class.create({
   initialize: function(callback, frequency) {
     this.callback = callback;
     this.frequency = frequency;
-    this.currentlyExecuting = false
+    this.currentlyExecuting = false;
+
+    this.registerCallback();
+  },
+
+  registerCallback: function() {
+    this.timer = setInterval(this.onTimerEvent.bind(this), this.frequency * 1000);
+  },
+
+  execute: function() {
+    this.callback(this);
+  },
+
+  stop: function() {
+    if (!this.timer) return;
+    clearInterval(this.timer);
+    this.timer = null;
+  },
+
+  onTimerEvent: function() {
+    if (!this.currentlyEx

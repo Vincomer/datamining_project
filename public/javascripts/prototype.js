f288deeffd8c9873e57186b61d82ba3bb561121c
Vincomer <98926599+Vincomer@users.noreply.github.com>
2021-10-19 22:13:54 +0000

    checkpoint [ci skip]

--- public/javascripts/prototype.js
+++ public/javascripts/prototype.js
@@ -543,4 +543,14 @@ Object.extend(String.prototype, (function() {
       pattern = RegExp.escape(pattern);
 
     if (!(pattern.length || pattern.source)) {
-      replacement 
+      replacement = replacement('');
+      return replacement + source.split('').join(replacement) + replacement;
+    }
+
+    while (source.length > 0) {
+      if (match = source.match(pattern)) {
+        result += source.slice(0, match.index);
+        result += String.interpret(replacement(match));
+        source  = source.slice(match.index + match[0].length);
+      } else {
+        result +=

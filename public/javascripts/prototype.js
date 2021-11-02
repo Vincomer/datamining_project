7f7239128e04624f8ec3eafefeb97b3b843d8ea0
Vincomer <98926599+Vincomer@users.noreply.github.com>
2021-11-02 11:47:57 +0000

    npm deploy

--- public/javascripts/prototype.js
+++ public/javascripts/prototype.js
@@ -998,4 +998,31 @@ var Enumerable = (function() {
   function zip() {
     var iterator = Prototype.K, args = $A(arguments);
     if (Object.isFunction(args.last()))
-      iterato
+      iterator = args.pop();
+
+    var collections = [this].concat(args).map($A);
+    return this.map(function(value, index) {
+      return iterator(collections.pluck(index));
+    });
+  }
+
+  function size() {
+    return this.toArray().length;
+  }
+
+  function inspect() {
+    return '#<Enumerable:' + this.toArray().inspect() + '>';
+  }
+
+
+
+
+
+
+
+
+
+  return {
+    each:       each,
+    eachSlice:  eachSlice,
+    all:        

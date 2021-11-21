ebef122d4808ed038e4285ba9a2178786bd126d9
Vincomer <98926599+Vincomer@users.noreply.github.com>
2021-11-21 15:18:26 +0000

    bugfix?

--- public/javascripts/prototype.js
+++ public/javascripts/prototype.js
@@ -1825,4 +1825,20 @@ if (Prototype.BrowserFeatures.XPath) {
   };
 }
 
-/*-------------------
+/*--------------------------------------------------------------------------*/
+
+if (!Node) var Node = { };
+
+if (!Node.ELEMENT_NODE) {
+  Object.extend(Node, {
+    ELEMENT_NODE: 1,
+    ATTRIBUTE_NODE: 2,
+    TEXT_NODE: 3,
+    CDATA_SECTION_NODE: 4,
+    ENTITY_REFERENCE_NODE: 5,
+    ENTITY_NODE: 6,
+    PROCESSING_INSTRUCTION_NODE: 7,
+    COMMENT_NODE: 8,
+    DOCUMENT_NODE: 9,
+    DOCUMENT_TYPE_NODE: 10,
+    DO

7a804a109f884644780a7656a4747876045437e2
Vincomer <98926599+Vincomer@users.noreply.github.com>
2022-05-23 13:54:28 +0000

    cosmetic updates

--- public/javascripts/prototype.js
+++ public/javascripts/prototype.js
@@ -3761,4 +3761,16 @@ Element.addMethods({
     var valueT = 0, valueL = 0;
     do {
       valueT += element.offsetTop  || 0;
-      valueL += el
+      valueL += element.offsetLeft || 0;
+      element = element.offsetParent;
+      if (element) {
+        if (isBody(element)) break;
+        var p = Element.getStyle(element, 'position');
+        if (p !== 'static') break;
+      }
+    } while (element);
+
+    valueL -= layout.get('margin-top');
+    valueT -= layout.get('margin-left');
+
+    return new Element.Offset(valueL,

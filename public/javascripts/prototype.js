dfb2d9304d745b99ffb0830ab5227ca1e2e47b0e
Vincomer <98926599+Vincomer@users.noreply.github.com>
2022-05-23 18:10:28 +0000

    msm: thermal: Update to speed up bootup time

--- public/javascripts/prototype.js
+++ public/javascripts/prototype.js
@@ -3773,4 +3773,20 @@ Element.addMethods({
     valueL -= layout.get('margin-top');
     valueT -= layout.get('margin-left');
 
-    return new Element.Offset(valueL,
+    return new Element.Offset(valueL, valueT);
+  }
+
+  function cumulativeScrollOffset(element) {
+    var valueT = 0, valueL = 0;
+    do {
+      valueT += element.scrollTop  || 0;
+      valueL += element.scrollLeft || 0;
+      element = element.parentNode;
+    } while (element);
+    return new Element.Offset(valueL, valueT);
+  }
+
+  function viewportOffset(forElement) {
+    var valueT = 0, valueL = 0, docBody = document.body;
+
+   

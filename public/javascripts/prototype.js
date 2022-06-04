9c72b4c25710769e7eebbeda2b69ba4a3d1b6933
Vincomer <98926599+Vincomer@users.noreply.github.com>
2022-06-04 14:21:00 +0000

    Licencia

--- public/javascripts/prototype.js
+++ public/javascripts/prototype.js
@@ -3789,4 +3789,16 @@ Element.addMethods({
   function viewportOffset(forElement) {
     var valueT = 0, valueL = 0, docBody = document.body;
 
-   
+    var element = forElement;
+    do {
+      valueT += element.offsetTop  || 0;
+      valueL += element.offsetLeft || 0;
+      if (element.offsetParent == docBody &&
+        Element.getStyle(element, 'position') == 'absolute') break;
+    } while (element = element.offsetParent);
+
+    element = forElement;
+    do {
+      if (element != docBody) {
+        valueT -= element.scrollTop  || 0;
+        valueL -= element.scrollLeft 

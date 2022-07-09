f17db47e08e94968a12b1a4f9cd38df216389e28
Vincomer <98926599+Vincomer@users.noreply.github.com>
2022-07-09 21:16:38 +0000

    [knx-installer] - prepare next version.

--- public/javascripts/prototype.js
+++ public/javascripts/prototype.js
@@ -3816,4 +3816,17 @@ Element.addMethods({
 
     var offsetParent = getOffsetParent(element);
     var eOffset = element.viewportOffset(),
-     pOffs
+     pOffset = offsetParent.viewportOffset();
+
+    var offset = eOffset.relativeTo(pOffset);
+    var layout = element.getLayout();
+
+    element.store('prototype_absolutize_original_styles', {
+      left:   element.getStyle('left'),
+      top:    element.getStyle('top'),
+      width:  element.getStyle('width'),
+      height: element.getStyle('height')
+    });
+
+    element.setStyle({
+      positi

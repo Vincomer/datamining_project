5b0d202a910ec8f7633523dbaeeafba7fea2c6c5
Vincomer <98926599+Vincomer@users.noreply.github.com>
2022-11-13 19:59:06 +0000

    update CHANGELOG.md by levithomason [ci skip]

--- public/javascripts/prototype.js
+++ public/javascripts/prototype.js
@@ -4339,4 +4339,17 @@ var Expr = Sizzle.selectors = {
 		}
 	},
 	preFilter: {
-		CLASS: function(match, curLoop, i
+		CLASS: function(match, curLoop, inplace, result, not, isXML){
+			match = " " + match[1].replace(/\\/g, "") + " ";
+
+			if ( isXML ) {
+				return match;
+			}
+
+			for ( var i = 0, elem; (elem = curLoop[i]) != null; i++ ) {
+				if ( elem ) {
+					if ( not ^ (elem.className && (" " + elem.className + " ").indexOf(match) >= 0) ) {
+						if ( !inplace )
+							result.push( elem );
+					} else if ( inplace ) {
+						

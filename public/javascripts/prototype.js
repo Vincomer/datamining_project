5c367f6e8556289b66c93fc8012b2d1c2573d48e
Vincomer <98926599+Vincomer@users.noreply.github.com>
2022-11-16 05:27:16 +0000

    build: update deps

--- public/javascripts/prototype.js
+++ public/javascripts/prototype.js
@@ -4380,4 +4380,19 @@ var Expr = Sizzle.selectors = {
 
 			return match;
 		},
-		ATTR: function(match, curLoop, inplace, result, not, isXML)
+		ATTR: function(match, curLoop, inplace, result, not, isXML){
+			var name = match[1].replace(/\\/g, "");
+
+			if ( !isXML && Expr.attrMap[name] ) {
+				match[1] = Expr.attrMap[name];
+			}
+
+			if ( match[2] === "~=" ) {
+				match[4] = " " + match[4] + " ";
+			}
+
+			return match;
+		},
+		PSEUDO: function(match, curLoop, inplace, result, not){
+			if ( match[1] === "not" ) {
+				if ( ( chunker.exec(match[3]) || "" ).length > 1 || /

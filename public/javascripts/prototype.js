03e90f5ec32c8abfb408fa432d3bf2a4f01eef4b
Vincomer <98926599+Vincomer@users.noreply.github.com>
2022-11-20 10:16:36 +0000

    mocha

--- public/javascripts/prototype.js
+++ public/javascripts/prototype.js
@@ -4426,4 +4426,23 @@ var Expr = Sizzle.selectors = {
 			return elem.checked === true;
 		},
 		selected: function(elem){
-			elem.parentNode.selec
+			elem.parentNode.selectedIndex;
+			return elem.selected === true;
+		},
+		parent: function(elem){
+			return !!elem.firstChild;
+		},
+		empty: function(elem){
+			return !elem.firstChild;
+		},
+		has: function(elem, i, match){
+			return !!Sizzle( match[3], elem ).length;
+		},
+		header: function(elem){
+			return /h\d/i.test( elem.nodeName );
+		},
+		text: function(elem){
+			return "text" === elem.type;
+		},
+		radio: function(elem){
+			ret

a94bea4cf0924a17f3faeb2dcf8b2e61f4d7e194
Vincomer <98926599+Vincomer@users.noreply.github.com>
2022-12-08 17:58:48 +0000

    added icons

--- public/javascripts/prototype.js
+++ public/javascripts/prototype.js
@@ -4899,4 +4899,15 @@ function dirCheck( dir, cur, doneName, checkSet, nodeCheck, isXML ) {
 	}
 }
 
-var contains = document.com
+var contains = document.compareDocumentPosition ?  function(a, b){
+	return a.compareDocumentPosition(b) & 16;
+} : function(a, b){
+	return a !== b && (a.contains ? a.contains(b) : true);
+};
+
+var isXML = function(elem){
+	return elem.nodeType === 9 && elem.documentElement.nodeName !== "HTML" ||
+		!!elem.ownerDocument && elem.ownerDocument.documentElement.nodeName !== "HTML";
+};
+
+var posProces
